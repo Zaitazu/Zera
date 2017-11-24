8722f0828b53666bbc4d3b0294a8f465089fe56a
Zaitazu <andyschyns@outlook.com>
2017-11-24 22:38:33 +0100

    Add files via upload

---
+++ MusicBotFusion/musicbot/Troll.py
@@ -0,0 +1,14 @@
+trolls = [
+    "{0.mention}, tu ne casses pas trois pattes à un piou",
+    "A choisir entre {0.mention} et un blop, je préfère largement le blop.",
+    "{0.mention}, si j'avais ta tête à la place de mon cul, j'aurais honte de chier",
+    "{0.mention}, tu es aussi beau que tu es intelligent",
+    "{0.mention} a été bercé trop près du mur",
+    "{0.mention} a été démoulé trop tôt",
+    "{0.mention} a été fini à la pisse",
+    "Si {0.mention} était un feuilleton il s'appellerait Derrick",
+    "{0.mention}, die in a fire and reroll IRL with more int",
+    "Et si {0.mention} allait se cacher dans son slip ?",
+    "{0.mention}, va jouer avec ta quéquette dans le sable",
+    "{0.mention}, mes couilles sur ton nez, ça fait des rayban?"
+]
